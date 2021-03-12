999f7439f765f9ddc11ded839fa3915df54dfe51
NelRamirez <NelRamirez49@gmail.com>
2021-03-11 20:29:23 -0500

    new file git added

---
+++ src/testTheGit/git.java
@@ -0,0 +1,13 @@
+package testTheGit;
+
+public class git {
+	public static void main(String[] args ) {
+		
+		System.out.println("This is git");
+		
+		System.out.println("We are learning git");
+		
+		
+	}
+
+}
